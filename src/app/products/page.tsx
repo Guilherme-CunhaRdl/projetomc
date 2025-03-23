dd6fb0055032a8f7e06ec74837fe00fe222d7c86
Guilherme-CunhaRdl <guilhermecunha928@gmail.com>
2025-03-23 14:32:04 -0300

    feat: add shadcn components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import { Button }from '@/components/ui/button';
+
+const ProductPage = () => {
+    return <div className="p-5 border border-red-500  rounded-xl">
+    <h1 className="text-red-500">Teste</h1>
+    <Button>merda</Button>    
+    </div>;
+}
+ 
+export default ProductPage;
